232a511677eccec8f5eb3fa92bca9dfc3640b548
nixni <2062320169@qq.com>
2018-06-15 11:58:45 +0800

    Initial commit

---
+++ grab_links.js
@@ -0,0 +1,38 @@
+console.log('Multi Click started');
+
+links=[]
+sections = document.getElementsByClassName("r")
+
+for (var i = 0; i < sections.length; i++) {
+	link=sections[i].getElementsByTagName('a')[0].href
+    //console.log(link); 
+    links = links.concat(link);
+}
+console.log(links)
+
+links_json=JSON.stringify(links);
+//console.log(links_json);
+
+window.addEventListener('click', function (evt) {
+	if (evt.detail === 3) {     
+		chrome.runtime.sendMessage({greeting:links_json}, function(response) {
+			console.log(response.farewell);
+		});
+		add_border(sections)
+
+	}
+});
+
+
+
+function add_border(sections){
+	for (var i = 0; i < sections.length; i++) {
+		sections[i].style.border = "1px solid red"
+	}
+}
+
+
+
+
+
+
